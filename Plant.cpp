2e1877de3878459d713c5f6f46d3e22e21976602
Bronwyn Gautier <bronwyngautier@Bronwyns-MBP.home.gateway>
2024-10-13 03:04:57 +1030

    Updated Plant files with basic class members

--- Plant.cpp
+++ Plant.cpp
@@ -1,1 +1,21 @@
 #include"Plant.h"
+#include <iostream>
+
+using namespace std;
+
+
+// Constructor to initialize a Plant object
+Plant::Plant(string n, int buyPrice, int sPrice)
+    : Item(n, buyPrice), sellPrice(sPrice) {}
+
+
+// Displays the plant's name and sell price
+void Plant::display() const {
+    cout << "Plant: " << name << ", Sell Price: " << sellPrice << " gold\n";
+}
+
+
+// Returns the sell price of the plant
+int Plant::getSellPrice() const {
+    return sellPrice;
+}
--- Plant.h
+++ Plant.h
@@ -2,8 +2,27 @@
 #define PLANT_H
 
 #include"Item.h"
+#include<string>
+#include<fstream>
 
-class Plant: public Item{};
+class Plant: public Item{
+    private:
+    int sellPrice; // The selling price of the plant
+
+
+    public:
+    // Constructor to initialize a Plant object
+    Plant(std::string n, int buyPrice, int sPrice);
+
+
+    // Displays the plant's name and sell price
+    void display() const override;
+
+
+    // Returns the sell price of the plant
+    int getSellPrice() const;
+
+};
 
 
 #endif // PLANT_H
