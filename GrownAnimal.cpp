2b2e92c59d07cf455b055bdae3a99c29949bc0b0
a1918274 <a1918274@student.adelaide.edu.au>
2024-10-13 23:14:26 +1030

    created implementations forall methods in grown animal header file in cpp file

--- GrownAnimal.cpp
+++ GrownAnimal.cpp
@@ -1,4 +1,54 @@
-#include"GrownAnimal.h"
-#include<iostream>
+#include "GrownAnimal.h"
+
+#include <iostream>
 
 using namespace std;
+
+// constructor to initialize a GrownAnimal object
+GrownAnimal::GrownAnimal(const std::string& n, int p, const std::string& t,
+                         int value)
+    : Animal(n, p, t), produceType(t), produceValue(value), produceCount(0) {}
+
+// display the grown animal's details including produce count (override)
+void GrownAnimal::display() const {
+  cout << "Grown Animal: " << name << ", Price: " << price
+       << " gold (Produce: " << produceType
+       << ", Produce count: " << produceCount << " )\n";
+}
+
+// generate produce and updates the count
+void GrownAnimal::produceItem() {
+  produceCount++;  // increment the produce count each time produce item is
+                   // called
+}
+
+// gets the current produce count
+int GrownAnimal::getProduceCount() const { return produceCount; }
+
+// clears the produce count
+void GrownAnimal::clearProduce() {
+  produceCount = 0;  // resets the produce count after selling
+}
+
+// gets the produce value
+int GrownAnimal::getProduceValue() const {
+  return produceValue;  // return the value of the produce
+}
+
+// serialize (override) and deserialize the grown animal to and from a file
+void GrownAnimal::serialize(std::ofstream& outFile) const {
+  outFile << "GrownAnimal: " << name << " " << price << " " << produceType
+          << " " << produceValue << " " << produceCount << "\n";
+}
+
+GrownAnimal* GrownAnimal::deserialize(std::ifstream& inFile) {
+  string name, produceType;
+  int price, produceValue, produceCount;
+
+  inFile >> name >> price >> produceType >> produceValue >> produceCount;
+
+  GrownAnimal* animal = new GrownAnimal(name, price, produceType, produceValue);
+  animal->produceCount = produceCount; // set the produce count directly
+  
+  return animal;
+}
