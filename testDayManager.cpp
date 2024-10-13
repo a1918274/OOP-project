ea2aedc9a16b944ef67ead43172410ef48328f51
a1918274 <a1918274@student.adelaide.edu.au>
2024-10-13 22:32:43 +1030

    completed testing for DayManager and removed a.out file

--- testDayManager.cpp
+++ testDayManager.cpp
@@ -7,10 +7,16 @@ using namespace std;
 
 class UnitTest {
  public:
-  void runTests() { testGetActions(); }
+  void runTests() {
+    testGetActions();
+    testSetActions();
+    testGetDay();
+    testSetDay();
+  }
 
  private:
   void testGetActions() {
+    cout << "Testing for getAction:" << endl;
     {
       DayManager dayManager(3);
       if (dayManager.getActions() != 3) {
@@ -35,32 +41,109 @@ class UnitTest {
     cout << "All tests passed!" << endl;
   }
 
-  //   // Get the current number of actions
-  //   int getActions() const;
+  void testSetActions() {
+    cout << "Testing for setAction:" << endl;
+    {
+      DayManager dayManager(1);
+      if (dayManager.getActions() != 1) {
+        cout << "Test 1 constructor failed!" << endl;
+      }
+      dayManager.setActions(0);
+      if (dayManager.getActions() != 0) {
+        cout << "Test 1 failed!" << endl;
+      }
+    }
 
-  //   // Set the current number of actions
-  //   void setActions(int actions);
+    {
+      DayManager dayManager(1);
+      if (dayManager.getActions() != 1) {
+        cout << "Test 2 constructor failed!" << endl;
+      }
+      dayManager.setActions(3);
+      if (dayManager.getActions() != 3) {
+        cout << "Test 2 failed!" << endl;
+      }
+    }
 
-  //   // Get the current day
-  //   int getDay() const;
+    {
+      DayManager dayManager(10);
+      if (dayManager.getActions() != 10) {
+        cout << "Test 3 constructor failed!" << endl;
+      }
+      dayManager.setActions(-3);
+      if (dayManager.getActions() != 0) {
+        cout << "Test 3 failed!" << endl;
+      }
+    }
 
-  //   // Set the current day
-  //   void setDay(int day);
+    cout << "All tests passed!" << endl;
+  }
 
-  //   // Reset actions for a new day
-  //   void resetActions(int newActions);
+  void testGetDay() {
+    cout << "Testing for getDay:" << endl;
 
-  //   // create an object of DayManager
-  //   DayManager day(3);
+    {
+      DayManager dayManager(1);
+      if (dayManager.getDay() != 0) {
+        cout << "Test 1 failed!" << endl;
+      }
+    }
 
-  //   void testAddition() {
-  //     Addition addition;
+    {
+      DayManager dayManager(3);
+      if (dayManager.getDay() != 0) {
+        cout << "Test 2 failed!" << endl;
+      }
+    }
 
-  //     if (addition.add(1, 2) != 3) {
-  //       std::cout << "Test 1 failed!" << std::endl;
-  //     }
-  //     // Add more tests here
-  //   }
+    {
+      DayManager dayManager(-3);
+      if (dayManager.getDay() != 0) {
+        cout << "Test 3 failed!" << endl;
+      }
+    }
+
+    cout << "All tests passed!" << endl;
+  }
+
+  void testSetDay() {
+    cout << "Testing for setDay:" << endl;
+
+    {
+      DayManager dayManager(1);
+      if (dayManager.getDay() != 0) {
+        cout << "Test 1 constructor failed!" << endl;
+      }
+      dayManager.setDay(3);
+      if (dayManager.getDay() != 3) {
+        cout << "Test 1 failed!" << endl;
+      }
+    }
+
+    {
+      DayManager dayManager(3);
+      if (dayManager.getDay() != 0) {
+        cout << "Test 2 constructor failed!" << endl;
+      }
+      dayManager.setDay(0);
+      if (dayManager.getDay() != 0) {
+        cout << "Test 2 failed!" << endl;
+      }
+    }
+
+    {
+      DayManager dayManager(-1);
+      if (dayManager.getDay() != 0) {
+        cout << "Test 3 constructor failed!" << endl;
+      }
+      dayManager.setDay(-1);
+      if (dayManager.getDay() != 0) {
+        cout << "Test 3 failed!" << endl;
+      }
+    }
+
+    cout << "All tests passed!" << endl;
+  }
 };
 
 int main() {
