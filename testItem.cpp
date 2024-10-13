56170bfd29ecf0d299f66f2efe5efadeb7da4c0a
Bronwyn Gautier <bronwyngautier@Bronwyns-MacBook-Pro.local>
2024-10-13 15:41:20 +1030

    Testing file for Item completed

--- testItem.cpp
+++ testItem.cpp
@@ -8,6 +8,6 @@ int main(){
     // As Item is an abstract class (due to pure virtual functions), an item
     // cannot be created, and thus the functions cannot be tested. Proof of testing
     // for this class can thus be found in child/inhered classes (please refer to 
-    //GrownAnimal, YoungAnimal, Plant and Seed classes for relevant testing).
+    // GrownAnimal, YoungAnimal, Plant and Seed classes for relevant testing).
 return 0;
 }
