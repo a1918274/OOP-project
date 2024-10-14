8fceaf6c7d88109cf8918b6d015214b5dc076b10
a1918274 <a1918274@student.adelaide.edu.au>
2024-10-14 23:35:30 +1030

    formatting and commenting for weather.h

--- Weather.h
+++ Weather.h
@@ -1,41 +1,45 @@
 #ifndef WEATHER_H
 #define WEATHER_H
 
-#include "DayManager.h"
+#include <cstdlib>  // For rand() function
 #include <string>
-#include <cstdlib> //Library is used for rand() function
 
-// This class is responsible for managing the random weather system, 
-// wherein each day has a 75% chance of being sunny, 20% chance of rain, 
+#include "DayManager.h"
+
+// Weather class represents the weather system in the game,
+// wherein each day has a 75% chance of being sunny, 20% chance of rain,
 // or 5% chance of an earthquake occurring.
 
 class Weather {
-public:
-    // Sets each type of weather to 0, 1 and 2 respectively
-    enum Type { SUNNY, RAIN, EARTHQUAKE };
+ public:
+  // Sets each type of weather to 0, 1 and 2 respectively
+  enum Type { SUNNY, RAIN, EARTHQUAKE };
 
-private:
-    // Stores the current weather type
-    Type currentWeather;
+ private:
+  // Stores the current weather type
+  Type currentWeather;
 
-public:
-    // Constructor to initialize the weather
-    Weather();
+ public:
+  // Constructor to initialize the weather
+  Weather();
 
-    // Returns the current weather type
-    Type getWeather() const;      
-    // Returns a formatted string representing the current weather          
-    std::string getWeatherString() const;
+  // Getters
+  // Returns the current weather type
+  Type getWeather() const;
+  // Returns a formatted string representing the current weather
+  std::string getWeatherString() const;
 
-    // Sets the current weather type
-    void setWeather(Type weather);
+  // Setter
+  // Sets the current weather type
+  void setWeather(Type weather);
 
-    // Determines weather at the start of each day
-    void generateWeather();
+  // Methods
+  // Determines weather at the start of each day
+  void generateWeather();
 
-    // Applies the effects of the current weather on the player's actions
-    void applyWeatherEffects(DayManager& dayManager);  
+  // Effect on Gameplay
+  // Applies the effects of the current weather on the player's actions
+  void applyWeatherEffects(DayManager& dayManager);
 };
 
-
-#endif // WEATHER_H
+#endif  // WEATHER_H
