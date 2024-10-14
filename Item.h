074469d90a6b350238e817e5e9489e4c451dc47d
a1918274 <a1918274@student.adelaide.edu.au>
2024-10-14 10:35:20 +1030

    formatted item.h

--- Item.h
+++ Item.h
@@ -1,42 +1,41 @@
 #ifndef ITEM_H
 #define ITEM_H
 
+#include <fstream>
 #include <iostream>
 #include <string>
-#include <fstream>
 
-//Abstract class that contains the information about each ‘item’, specifically 
-//name and price (i.e. the name and price of either a crop or animal). 
-//The class also contains a pure virtual function for displaying item information.
+// Abstract class that contains the information about each ‘item’, specifically
+// name and price (i.e. the name and price of either a crop or animal).
+// The class also contains a pure virtual function for displaying item
+// information.
 
 class Item {
-    protected: //The user should not be able to change the name or price of items
-    std::string name; //The name of the item
-    int price; //The price of the item
+ protected:  // The user should not be able to change the name or price of items
+  std::string name;  // The name of the item
+  int price;         // The price of the item
 
-    public:
-    //Constructor
-    Item(const std::string& n, int p);
+ public:
+  // Constructor
+  Item(const std::string& n, int p);
 
-    //Virtual Destructor
-    virtual ~Item(); 
+  // Virtual Destructor
+  virtual ~Item();
 
-    //Pure virtual item for displaying item details
-    virtual void display() const = 0;
+  // Pure virtual item for displaying item details
+  virtual void display() const = 0;
 
-    //Pure virtual function for serialization 
-    virtual void serialize(std::ofstream& outFile) const = 0;
+  // Pure virtual function for serialization
+  virtual void serialize(std::ofstream& outFile) const = 0;
 
-    //Static method for deserialization
-    static Item* deserialize(std::ifstream& inFile);
+  // Static method for deserialization
+  static Item* deserialize(std::ifstream& inFile);
 
-    //Getter for item name
-    std::string getName() const;
+  // Getter for item name
+  std::string getName() const;
 
-    //Getter for item price
-    int getPrice() const;
-
+  // Getter for item price
+  int getPrice() const;
 };
 
-
-#endif  //ITEM_H
+#endif  // ITEM_H
