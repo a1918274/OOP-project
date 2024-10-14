a40d713e2bc9f0f5438ce60849ea30fbdb3d15af
Jarryd Wong <a1742622@adelaide.edu.au>
2024-10-14 12:44:16 +1030

    Improved comment clarity for MainMenu.cpp and MainMenu.h

--- MainMenu.cpp
+++ MainMenu.cpp
@@ -4,15 +4,16 @@
 
 using namespace std;
 
-// Constructor
+// Constructor to initialize the MainMenu with a Game instance
 MainMenu::MainMenu(Game* gameInstance) : game(gameInstance) {}
 
 // Displays the main menu
-void MainMenu:: displayMenu() {
+void MainMenu::displayMenu() {
     string input; // Change to string to handle input
     while (true) {
-        cout << "\nWelcome to Happy Life Farm :)\n\n";
-        cout << "select a number to being\n";
+        cout << "_____________________________________\n";
+        cout << "Welcome to Happy Life Farm :)\n\n";
+        cout << "Select a number to begin\n";
         cout << "1. New Game\n";
         cout << "2. Load Game\n";
         cout << "3. Quit Game\n";
@@ -22,28 +23,28 @@ void MainMenu:: displayMenu() {
         // Validate the input
         if (ValidNumberCheck::isValidNumber(input)) {
             int choice = stoi(input); // Convert to integer
-            handleSelection(choice);
+            handleSelection(choice);   // Handle the user's selection
         } else {
-            cout << "Invalid input. Please enter a number.\n";
+            cout << "Please stop trying to break the game :(\n"; // Error message for invalid input
         }
     }
 }
 
-// Handles menu selection
+// Handles the user's menu selection
 void MainMenu::handleSelection(int choice) {
     switch (choice) {
         case 1:
-            game->initializeGame();     //Initialize a new game
-            game->play();               //Start a new game
+            game->initializeGame();     // Initialize a new game
+            game->play();               // Start a new game
             break;
         case 2:
-            game->loadGame();           //Load the last saved game
-            game->play();               //Start the game with the loaded state
+            game->loadGame();           // Load the last saved game
+            game->play();               // Start the game with the loaded state
             break;
         case 3:
-            exit(0);                    //Quit the game
+            exit(0);                    // Quit the game
             break;
         default:
-            cout << "Invalid choice. Please try again.\n";
+            cout << "Invalid choice. Please try again.\n"; // Error message for invalid choice
     }
 }
