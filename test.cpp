fd8c30e4a5ea7a499f00dcd49231b39a2c3b2835
a1918274 <a1918274@student.adelaide.edu.au>
2024-10-14 23:27:36 +1030

    added testing for main menu but couldnt compile because of game.cpp not completed yet

--- test.cpp
+++ test.cpp
@@ -1,14 +1,18 @@
 // main file for testing all other test files
 // include all test files here
 #include "TestDayManager.h"
-#include"TestValidNumberCheck.h"
+#include "TestMainMenu.h"
+#include "TestValidNumberCheck.h"
 
 int main() {
-//   TestDayManager testDayManger;
-//   testDayManger.runTests();
+  //   TestDayManager testDayManger;
+  //   testDayManger.runTests();
 
-  TestValidNumberCheck testValidNumberCheck;
-  testValidNumberCheck.runTests();
+  //   TestValidNumberCheck testValidNumberCheck;
+  //   testValidNumberCheck.runTests();
+
+  TestMainMenu testMainMenu;
+  testMainMenu.runTests();
 
   return 0;
 }
