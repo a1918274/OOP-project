ca84e77ddc11efcf07f1c09cf13f8087fd14ce43
Bronwyn Gautier <bronwyngautier@Bronwyns-MacBook-Pro.local>
2024-10-15 11:07:03 +1030

    Updated animal test file

--- TestAnimal.h
+++ TestAnimal.h
@@ -1,16 +1,16 @@
+#ifndef ANIMAL_H
+#define ANIMAL_H
+
 // testing for Animal
 
 #include <iostream>
 
 #include "Animal.h"
 
-using namespace std;
 
-int main() {
   // object cannot be created for animal class as it is an abstract class (has
   // pure virtual function)
 
   // testing for Animal class is in the child class (YoungAnimal & GrownAnimal)
 
-  return 0;
-}
+#endif //ANIMAL_H
