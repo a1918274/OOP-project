7b5c8591ff6cc50b32cfeadba26238d2d7949785
a1918274 <a1918274@student.adelaide.edu.au>
2024-10-15 18:25:23 +1030

    commenting for shop.h

--- Shop.h
+++ Shop.h
@@ -1,27 +1,27 @@
 #ifndef SHOP_H
 #define SHOP_H
+
 #include "Inventory.h"
 
-//This class includes the mechanisms by which 
-//the player can buy items with their gold
+// Shop class represents a shop where the player can buy items with
+// their gold.
 
-// Class representing a shop where items can be bought
-class Shop{
-    private:
-    Inventory inventory; // Inventory for the shop
+class Shop {
+ private:
+  Inventory inventory;  // Inventory for the shop
 
-    public:
-    // Constructor
-    Shop();                 // Initializes the shop's inventory
+ public:
+  // Constructor
+  Shop();  // Initializes the shop's inventory
 
-    // Destructor
-    ~Shop();                    // Cleans up dynamically allocated items
+  // Destructor
+  ~Shop();  // Cleans up dynamically allocated items
 
-    // Methods
-    void displayItems() const;  // Function to display available items in the shop
-    bool buyItem(int choice, int& gold, Inventory& playerInventory);
-    // Function that allows a player to buy an item from the shop, 
-    // Deducts their gold accordingly, and adds the item to the player's inventory
+  // Methods
+  void displayItems() const;  // Function to display available items in the shop
+  bool buyItem(int choice, int& gold, Inventory& playerInventory);
+  // Function that allows a player to buy an item from the shop,
+  // Deducts their gold accordingly, and adds the item to the player's inventory
 };
 
-#endif //SHOP_H
+#endif  // SHOP_H
---
+++ TestShop.h
@@ -0,0 +1,13 @@
+#ifndef TESTSHOP_H
+#define TESTSHOP_H
+
+// Testing for Shop
+
+#include <algorithm>
+#include <iostream>
+
+#include "Shop.h"
+
+using namespace std;
+
+#endif  // TESTSHOP_H
