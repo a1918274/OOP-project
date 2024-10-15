886190d24461838e3cb51fc6722fe6dd66cccd8b
a1918274 <a1918274@student.adelaide.edu.au>
2024-10-15 16:56:13 +1030

    test for dog but not working for serialization and deserialization

--- TestDog.h
+++ TestDog.h
@@ -15,9 +15,11 @@ using namespace std;
 class TestDog {
  public:
   void runTests() {
+    cout << "\nStart testing for Dog:\n\n";
     testConstructor();
     testDisplay();
     testSerializeDeserialize();
+    cout << "\nFinished testing for Dog!\n\n";
   }
 
  private:
@@ -43,14 +45,86 @@ class TestDog {
   }
 
   void testDisplay() {
+    cout << "Testing for Display:\n";
+
     {
-      Dog dog("dog", 9);
+      Dog dog("Happy", 100);
+      cout << "Expected output: [ 1x ] Dog: Happy (100 gold)\n";
+      cout << "Actual output: ";
       dog.display();
       cout << "Test 1 passed!" << endl;
     }
+
+    {
+      Dog dog("Doggie", 0);
+      cout << "Expected output: [ 1x ] Dog: Doggie (0 gold)\n";
+      cout << "Actual output: ";
+      dog.display();
+      cout << "Test 2 passed!" << endl;
+    }
   }
 
-  void testSerializeDeserialize() {}
+  void testSerializeDeserialize() {
+    cout << "Testing for serialization and deserialization:\n";
+    {
+      // serialization
+      Dog dog("doggie", 100);
+      std::ofstream outFile("testDog1.txt");
+      assert(outFile);  // Error if outFile is not created
+      dog.serialize(outFile);
+      outFile.close();
+
+      // deserialization
+      std::ifstream inFile("testDog1.txt");
+      assert(inFile);  // Error if inFile cannot open
+      Dog* deserializedDog = Dog::deserialize(inFile);
+      assert(deserializedDog->getName() == "doggie" &&
+             deserializedDog->getPrice() == 100 &&
+             deserializedDog->getItemCount() == 1);
+      delete deserializedDog;
+      inFile.close();
+    }
+
+    {
+      // serialization
+      Dog dog("zero", 0);
+      std::ofstream outFile("testDog.txt");
+      // assert(outFile);  // Error if outFile is not created
+      dog.serialize(outFile);
+      outFile.close();
+
+      // deserialization
+      std::ifstream inFile("testDog.txt");
+      // assert(inFile);  // Error if inFile cannot open
+      Dog* deserializedDog = Dog::deserialize(inFile);
+      assert(deserializedDog->getName() == "zero" &&
+             deserializedDog->getPrice() == 0 &&
+             deserializedDog->getItemCount() == 1);
+      delete deserializedDog;
+      inFile.close();
+    }
+
+    {
+      // serialization
+      Dog dog("yappie", 50);
+      std::ofstream outFile("testDog.txt");
+      // assert(outFile);  // Error if outFile is not created
+      dog.serialize(outFile);
+      outFile.close();
+
+      // deserialization
+      std::ifstream inFile("testDog.txt");
+      // assert(inFile);  // Error if inFile cannot open
+      Dog* deserializedDog = Dog::deserialize(inFile);
+      assert(deserializedDog->getName() == "yappie" &&
+             deserializedDog->getPrice() == 50 &&
+             deserializedDog->getItemCount() == 1);
+      delete deserializedDog;
+      inFile.close();
+    }
+
+    cout << "Test serialization and deserialization passed!" << endl;
+  }
 };
 
 #endif  // TESTDOG_H
