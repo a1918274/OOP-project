6cb779ba7fe2c5f36925b9147a7e9481731bdd18
a1918274 <a1918274@student.adelaide.edu.au>
2024-10-15 10:34:32 +1030

    renamed test animal from cpp to h, formatting and commenting test animal.h

---
+++ TestAnimal.h
@@ -0,0 +1,13 @@
+#ifndef TESTANIMAL_H
+#define TESTANIMAL_H
+
+// Testing for Animal
+
+#include "Animal.h"
+
+// object cannot be created for animal class as it is an abstract class (has
+// pure virtual function)
+
+// testing for Animal class is in the child class (YoungAnimal & GrownAnimal)
+
+#endif  // TESTANIMAL_H
